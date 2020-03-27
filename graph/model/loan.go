014555eb854eca669c0c72d464d986cadc12fd70
Aaron Qian <aq1018@gmail.com>
2020-03-27 03:30:02 -0700

    Use an array of pointers

--- graph/model/loan.go
+++ graph/model/loan.go
@@ -8,7 +8,7 @@ import (
 
 type Loan struct {
 	ID         int64           `json:"id" gorm:"primary_key;type: serial;"`
-	Properties []Property      `json:"properties" gorm:"many2many:loan_properties"`
+	Properties []*Property     `json:"properties" gorm:"many2many:loan_properties"`
 	Valuation  decimal.Decimal `json:"valuation" sql:"type:decimal(16,2)"`
 	Principal  decimal.Decimal `json:"principal" sql:"type:decimal(16,2)"`
 	Interest   decimal.Decimal `json:"interest" sql:"type:decimal(8,6)"`
